edd537fe0d7f72b940496713e7f08b3f3ce4facf
GShotwell <gordon@crunch.io>
2019-01-26 11:15:21 -0400

    Documentation, checks

--- R/parserd.R
+++ R/parserd.R
@@ -1,9 +1,12 @@
 package_docs <- function(pkg) {
   help_dir <- system.file("help", package = pkg)
+  if (help_dir == "") {
+      stop("No help files found for", pkg, "do you have the package installed?")
+  }
   db_path <- file.path(help_dir, pkg)
   docs <- tools:::fetchRdDB(db_path)
   out <- docs %>%
-      map_df(~parse_document_list(., pkg))
+      purrr::map_df(~parse_document_list(., pkg))
   return(out)
 }
 
@@ -21,6 +24,19 @@ parse_document_list <- function(doc, pkg) {
     return(out)
 }
 
+#' Generate a dataframe for anki cards
+#'
+#' This generates a dataframe of anki cards. The front of the card is
+#' the function name, the back is the title, description, and examples.
+#' Note that you need to have the package installed for this to work.
+#'
+#' @param pkg The package whose functions you want to memorize
+#'
+#' @return A dataframe
+#' @export
+#'
+#' @examples
+#' generate_anki_df("purrr")
 generate_anki_df <- function(pkg) {
     docs <- package_docs(pkg)
     out <- tibble::data_frame(
@@ -32,6 +48,20 @@ generate_anki_df <- function(pkg) {
     return(out)
 }
 
+
+#' Write an anki csv
+#'
+#' This writes an anki csv which can then be imported into the anki app
+#'
+#' @param pkg The name of the package whose functions you would like to
+#' memorize
+#' @param path Path for the exported csv
+#'
+#' @return Nothing, called for its side effects.
+#' @export
+#'
+#' @examples
+#' write_anki_df("purrr", "purrr.csv")
 write_anki_df <- function(pkg, path) {
-    write.csv(generate_anki_df(pkg), path)
+    utils::write.csv(generate_anki_df(pkg), path)
 }
--- README.md
+++ README.md
@@ -1,20 +1,24 @@
 # ankify
 
-The goal of ankify is to generate anki decks from R packages. The 
+The goal of ankify is to generate anki decks from R packages. This
+is to assist in learning R functions using spaced repitition. For more
+information on Anki visit the [Anki Website](https://apps.ankiweb.net/). For more on using spaced repitition to learn programming see this Michael Nielson [article](http://augmentingcognition.com/ltm.html). 
 
 ## Installation
 
 You can install the released version of ankify from [CRAN](https://CRAN.R-project.org) with:
 
 ``` r
-install.packages("ankify")
+install.packages("devtools")
+devtools::install_github("GShotwell/ankify")
 ```
 
 ## Example
 
-This is a basic example which shows you how to solve a common problem:
+The package contains a few functions to parse a package's documentation into a dataframe. This is useful for creating a dataframe of documentation entries which you can then immport into anki. 
 
-``` r
-## basic example code
+```
+df <- generate_anki_df(purrr) 
+# You can also write the csv in one step with `write_anki_df`
 ```
 
